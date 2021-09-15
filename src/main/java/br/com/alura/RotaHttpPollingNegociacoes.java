149ffa1c3017c2fba241023de1fb20ebdbf195dc
Robson <robmartinhao@gmail.com>
2021-09-14 21:23:12 -0300

    Desafio Parte 2: XStreamDataFormat e split

--- src/main/java/br/com/alura/RotaHttpPollingNegociacoes.java
+++ src/main/java/br/com/alura/RotaHttpPollingNegociacoes.java
@@ -1,26 +1,34 @@
 package br.com.alura;
 
+import com.thoughtworks.xstream.XStream;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xstream.XStreamDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class RotaServicoWebTimer {
+public class RotaHttpPollingNegociacoes {
 
     public static void main(String[] args) throws Exception {
 
         CamelContext context = new DefaultCamelContext();
 
+        final XStream xstream = new XStream();
+        xstream.alias("negociacao", Negociacao.class);
+
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
 
-                from("timer://negociacoes?fixedRate=true&delay=1s&period=360s").
-                        to("http4://argentumws-spring.herokuapp.com/negociacoes").
-                        convertBodyTo(String.class).
-                        log("${body}").
-                        setHeader(Exchange.FILE_NAME, constant("negociacoes.xml")).
-                        to("file:saida");
+                from("timer://negociacoes?fixedRate=true&delay=1s&period=360s")
+                        .to("http4://argentumws-spring.herokuapp.com/negociacoes")
+                        .convertBodyTo(String.class)
+                        .unmarshal(new XStreamDataFormat(xstream))
+                        .split(body())
+                        .log("${body}")
+//                        .setHeader(Exchange.FILE_NAME, constant("negociacoes.xml"))
+//                        .to("file:saida");
+                        .end();
             }
         });
         context.start();
