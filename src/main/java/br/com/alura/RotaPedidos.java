5f5caeef5c14548a7341c2bce1461c84466c9432
Robson <robmartinhao@gmail.com>
2021-09-30 21:02:29 -0300

    Validação de mensagens e tratamento de erros

--- src/main/java/br/com/alura/RotaPedidos.java
+++ src/main/java/br/com/alura/RotaPedidos.java
@@ -2,6 +2,7 @@ package br.com.alura;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -17,31 +18,66 @@ public class RotaPedidos {
             @Override
             public void configure() throws Exception {
 
+                errorHandler(deadLetterChannel("file:erro")
+                        .logExhaustedMessageHistory(true)
+                        .maximumRedeliveries(3)
+                        .redeliveryDelay(2000)
+                        .onRedelivery(exchange -> {
+                            int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+                            int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+                            System.out.println("Redelivery " + counter + " / " + max);
+                        })
+                );
+
                 from("file:pedidos?delay=5s&noop=true").
+
                         routeId("rota-pedidos").
-                        multicast().
-                        to("direct:soap").
-                        to("direct:http");
+
+                        to("validator:pedido.xsd");
+//                        multicast().
+//                        to("direct:soap").
+//                        to("direct:http");
 
                 from("direct:http").
+
                         routeId("rota-http").
+
                         setProperty("pedidoId", xpath("/pedido/id/text()")).
+
                         setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
+
                         split().
+
                         xpath("/pedido/itens/item").
+
                         filter().
+
                         xpath("/item/formato[text()='EBOOK']").
+
                         setProperty("ebookId", xpath("/item/livro/codigo/text()")).
-                        marshal().xmljson().
+
+                        marshal().
+
+                        xmljson().
+
                         log("${id} - ${body}").
+
                         setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+
                         setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
+
                         to("http4://localhost:8080/webservices/ebook/item");
+
                 from("direct:soap").
+
                         routeId("rota-soap").
+
                         to("xslt:pedido-para-soap.xslt").
+
                         log("${id} - ${body}").
+
                         setHeader(Exchange.CONTENT_TYPE, constant("text/xml")).
+
                         to("http4://localhost:8080/webservices/financeiro");
             }
         });
